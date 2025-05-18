c9d9b6775526f9739f7866fd0a0790cd76255937
Tobias Grah <tobias.grah@cirromed.de>
2025-05-18 20:02:41 +0200

    some more fixes...

--- src/network.py
+++ src/network.py
@@ -1,11 +1,13 @@
 import socket
 import time
+import pyttsx3
 
 def check_for_connection():
     if not is_connected():
+        say("Derzeit besteht keine Internetverbindung. Ich versuche mich zu verbinden")
         wait_for_connection()
     else:
-        print("Netzverbindung besteht")
+        say("Netzverbindung erfolgreich")
 
 def is_connected():
     try:
@@ -17,3 +19,16 @@ def is_connected():
 def wait_for_connection():
     while not is_connected():
         time.sleep(2)
+
+
+engine = pyttsx3.init()
+
+def say(text):
+    engine.getProperty('voices')
+
+    engine.setProperty('pitch', 40)
+    engine.setProperty('rate', 130)
+    engine.setProperty('voice','german')
+    engine.setProperty('text','normalize')
+    engine.say(text)
+    engine.runAndWait()
