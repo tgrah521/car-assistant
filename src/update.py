7de1d333e92f175ae373058ef9a262d5d83477c8
tgrah521 <tgrah2001@web.de>
2025-05-25 21:58:04 +0200

    fix

--- src/update.py
+++ src/update.py
@@ -3,4 +3,4 @@ import subprocess
 
 say("Eine neue Version wurde installiert")
 print("Eine neue Version wurde installiert")
-subprocess.Popen(["python", "main.py"])
+subprocess.Popen(["python3", "main.py"])
