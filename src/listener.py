de2347fff544fc54bd8022ab2515438b43da25d2
tgrah521 <tgrah2001@web.de>
2025-05-28 22:45:53 +0200

    inconsistent space or tab

--- src/listener.py
+++ src/listener.py
@@ -56,7 +56,7 @@ def handle_voice_command():
         if listen():
             close_all_vlc()
             #play_mp3(HEARING_MP3, 2)
-		    action = recognize_text("ja?")
+            action = recognize_test("ja?")
             if "1X" in action:
                 action = action.replace('1X ', '')
                 action = recognize_text(f"{action}. Bitte Wiederhole")
