76449e7a74c9fa5cbfd1406527c6659cf544f296
tgrah521 <tgrah2001@web.de>
2025-05-30 21:41:34 +0200

    removed some shell script

--- resource/copy.sh
+++
@@ -1,9 +0,0 @@
-mv /home/swt/start-car_assistant.sh /home/swt/start_car_assistant_old.sh
-
-cp /home/swt/git/car-assistant/resource/start_car_assistant.sh /home/swt
-
-rm /home/swt/start_car_assistant_old.sh
-
-chmod +x /home/swt/start_car_assistant.sh
-
-mkdir /home/swt/git/car-assistant/resource/playlists
