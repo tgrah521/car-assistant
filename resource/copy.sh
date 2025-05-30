9f5d46faaba22624ee266464143f7f9a0988fd1c
Tobias Grah <tobias.grah@cirromed.de>
2025-05-30 12:54:58 +0200

    network check in start_car_assistant (deploy to headless pi)

---
+++ resource/copy.sh
@@ -0,0 +1,7 @@
+mv /home/tgrah/start-car_assistant.sh /home/tgrah/start_car_assistant_old.sh
+
+cp /home/tgrah/git/car-assistant/resource/start_car_assistant.sh /home/tgrah
+
+rm /home/tgrah/start_car_assistant_old.sh
+
+chmod +x /home/tgrah/start_car_assistant.sh
--- src/update.py
+++ src/update.py
@@ -3,3 +3,4 @@ import subprocess
 
 say("Eine neue Version wurde installiert")
 print("Eine neue Version wurde installiert")
+subprocess.run(['/home/tgrah/git/car-assistant/resource/copy.sh'])
